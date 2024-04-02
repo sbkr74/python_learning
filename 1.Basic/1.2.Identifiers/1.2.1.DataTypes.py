9d9284f5de314bc067062215ffbd9ab80faf447e
sbkr74 <sbkr74@gmal.com>
2024-04-02 15:07:07 +0530

    updated DataTypes.py

--- 1.Basic/1.2.Identifiers/1.2.1.DataTypes.py
+++ 1.Basic/1.2.Identifiers/1.2.1.DataTypes.py
@@ -6,3 +6,18 @@ int_val = int(input("Enter a integer value:"))
 
 # input as float value
 flt_val = float(input("Enter a float value:"))
+
+# input as boolean value
+bool_val = bool(input("Enter a boolean value:"))
+
+# input as Complex value
+comp_val = complex(input("Enter a complex value:"))
+
+# # input as float value
+# flt_val = float(input("Enter a float value:"))
+
+print("String: "+ str_val)
+print(int_val)
+print(flt_val)
+print(bool_val)
+print(comp_val)

bd7b8c45234249ed719319484b05589e5de6e98b
sbkr74 <sbkr74@gmal.com>
2024-04-05 20:18:00 +0530

    Renamed 1.2.1.DataTypes.py to 1.2.2.DataTypes.py

---
+++ 1.Basic/1.2.Identifiers/1.2.2.DataTypes.py
@@ -0,0 +1,28 @@
+# # input as string value
+# str_val = str(input("Enter a string value:"))
+
+# # input as integer value
+# int_val = int(input("Enter a integer value:"))
+
+# # input as float value
+# flt_val = float(input("Enter a float value:"))
+
+# # input as Complex value
+# comp_val = complex(input("Enter a complex value:"))
+
+# # input as boolean value
+# bool_val = bool(input("Enter a boolean value:"))
+
+#--------------------------------------------------------------------
+#--------------------------------------------------------------------
+
+# input as bytes value
+bytes_val = bytes(input("Enter a bytes value:"),encoding = 'utf-8')
+
+
+# print("String: "+ str_val)
+# print(int_val)
+# print(flt_val)
+# print(comp_val)
+# print(bool_val)
+print(bytes_val)

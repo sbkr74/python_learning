7ba816c52d62ad592c669e306a90afd1b7daf326
sbkr74 <sbkr74@gmal.com>
2024-04-05 20:19:35 +0530

    Added file 1.2.3.int_dtype.py

--- 1.Basic/1.2.Identifiers/1.2.2.Keywords.py
+++
@@ -1,5 +0,0 @@
-import keyword
-print(keyword.kwlist)
-keywords =['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-for i in keywords:
-    print(i)
---
+++ 1.Basic/1.2.Identifiers/1.2.3.int_dtype.py
@@ -0,0 +1,28 @@
+# We can represent int values in 4 ways
+# 1. Decimal form
+# 2. Binary form
+# 3. Octal form
+# 4. Hexa decimal form
+# ======================================
+# 1. Decimal form
+a = 10 
+print(a)
+print(type(a))
+
+# 2. Binary form
+# val = 0B123
+b = 5
+val = bin(b)
+print(val)
+print(type(val))
+
+# 3. Octal form
+# val = oct(0B1111)
+val = oct(0b111)
+print(val)
+print(type(val))
+
+# 4. Hexa decimal form
+val = hex(0B1111)
+print(val)
+print(type(val))

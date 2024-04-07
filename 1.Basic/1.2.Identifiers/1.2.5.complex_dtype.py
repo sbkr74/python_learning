1249627263f4a0f4742d4cebd3d72107a45f8172
sbkr74 <sbkr74@gmail.com>
2024-04-07 21:44:48 +0530

    1.2.5.complex_dtype().py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.5.complex_dtype.py
@@ -0,0 +1,7 @@
+a = 0b11+11.5j
+print(a)
+
+# b = 3.1157+0b11j
+# print(b)
+# we can assign decimal, binary, Octal, Hexadecimal to real part of complex number
+#for imaginary only decimal number can be assigned

c970c6ba25258a370ec4d992c3570c58b03ce123
sbkr74 <sbkr74@gmail.com>
2024-04-08 21:40:57 +0530

    1.2.6.bool_dtype.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.6.bool_dtype.py
@@ -0,0 +1,20 @@
+a = True
+print(type(a))
+print("a = ",a)
+
+i = 10
+j = 20
+c = j<i
+d = i<j
+print("c = ",c)
+print("d = ",d)
+
+#True + True = 2
+#True - False = 1
+k = a+d
+l = a-c
+print("k = (a+d) = ",k)
+print("k = (a-c) = ",l)
+
+
+

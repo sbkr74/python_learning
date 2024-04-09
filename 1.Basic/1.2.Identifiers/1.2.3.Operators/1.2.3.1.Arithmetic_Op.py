132c1f39a58498f7320239473e6aea2abc0cc71e
sbkr74 <sbkr74@gmail.com>
2024-04-09 23:22:26 +0530

    1.2.3.1.Arithmetic_Op.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.1.Arithmetic_Op.py
@@ -0,0 +1,19 @@
+# 1 -> + -> Addition
+# 2 -> - -> Substraction
+# 3 -> * -> Multiplication
+# 4 -> / -> Division
+# 5 -> % -> Modulus (rem) 
+# 6 -> // -> Floor Division
+# 7 -> ** -> Exponential operator
+
+a = 7 
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+

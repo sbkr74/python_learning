5ee12543dd10c385959c52b81dfe07ace5262455
sbkr74 <sbkr74@gmail.com>
2024-04-10 23:09:39 +0530

    1.2.2.6.str_dtype().py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.2.DataTypes/1.2.2.6.str_dtype().py
@@ -0,0 +1,31 @@
+#using single quote
+s = 'program'
+
+#using double quote
+t = "program"
+
+# use of triple single quote or triple double quote for multi-line string
+sample = '''python
+program'''
+sample2 = """another
+program"""
+
+#using triple quotes to use single quote and double code in string.
+st = 'you should "think" before you "speak".'
+st1 = '''you should "think" before you "speak".'''
+st2 = """checking for 'single quote'."""
+
+print(s)
+print(t)
+print(sample)
+print(sample2)
+print(st)
+print(st1)
+print(st2)
+
+#using single quote for commenting
+'''Here triple single qoutes are used for comments
+if the comments are in mult-line'''
+"""same goes for triple double quotes
+which can also used as multi-line comments."""
+'print(s)'

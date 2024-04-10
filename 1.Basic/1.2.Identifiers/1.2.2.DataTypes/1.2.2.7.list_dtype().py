a20fda223b5d6f3805ba9b431243f0760d5080ec
sbkr74 <sbkr74@gmail.com>
2024-04-10 23:48:27 +0530

    1.2.2.7.list_dtype().py file updated2.0

--- 1.Basic/1.2.Identifiers/1.2.2.DataTypes/1.2.2.7.list_dtype().py
+++ 1.Basic/1.2.Identifiers/1.2.2.DataTypes/1.2.2.7.list_dtype().py
@@ -1,11 +1,19 @@
 l1 = [7,7.5,3+5j,True,'program']
 print(l1)
 
+#printing value using list index
 print(l1[0])
 
+#printing range of values using list index range
 print(l1[1:4])
 
+#printing value using reverse list index
 print(l1[-1])
-
 print(l1[-3])
+
+#printing range of values using reverse index range
 print(l1[-4:-1])
+
+#using assignment operator changing value in list index
+l1[1] = 9.11
+print(l1[1])

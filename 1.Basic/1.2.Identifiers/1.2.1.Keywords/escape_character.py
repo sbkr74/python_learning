97c267e0b690e5fee9a1b45b4ef01cfd05583f73
sbkr74 <sbkr74@gmail.com>
2024-04-13 10:15:05 +0530

    escape_character.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.1.Keywords/escape_character.py
@@ -0,0 +1,17 @@
+# \n -> new line
+# \t -> horizontal tab
+# \r -> carriage return
+# \b -> backspace
+# \f -> form Feed
+# \v -> vertical tab
+# to use special character use \ and special character
+# \' -> single quote
+# \" -> Double quote
+# \\ -> backslash symbol
+
+print("Python program \nBasic to advance!")
+print("Python program from \f\fBasic to advance Tutorial.")
+print("Python program \v\vBasic to advance!")
+print("Python program \t\tBasic to advance!")
+print("Python program \r     Basic to advance!")
+print("Python program \n\t    \rBasic to advance!")

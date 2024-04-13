e39ef73a17fe5dffd5839a29f7eabc1445912c1b
sbkr74 <sbkr74@gmail.com>
2024-04-13 11:07:00 +0530

    1.2.3.4.Equality_Op.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.3.Equality_Op.py
@@ -0,0 +1,20 @@
+# == and != comes under equlaity operator
+# where == means equal which compares the values
+# and != means not equal
+# ===============================================================
+print(10==20)
+print(10!=20)
+print(10==False)
+print(False==False)
+print('python'=="python")
+print(10=='program')
+print()
+# ===============================================================
+a = 3+5j
+print(id(a))
+b = 3+5j
+print(id(b))
+
+print(a==b)
+print(a is b)
+

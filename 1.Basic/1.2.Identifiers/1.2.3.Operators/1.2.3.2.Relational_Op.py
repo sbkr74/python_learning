eb4d98f4fa461884b351e163d4af9a7a9c3a8e9c
sbkr74 <sbkr74@gmail.com>
2024-04-13 10:52:16 +0530

    1.2.3.2.Relational_Op.py file updated2.0

--- 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.2.Relational_Op.py
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.2.Relational_Op.py
@@ -2,3 +2,36 @@
 a = 10 
 b =20
 print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+
+print()
+# With string values
+
+s1 = 'python'
+s2 = 'python'
+print(s1>s2)
+print(s1>=s2)
+print(s1<s2)
+print(s1<=s2)
+
+print()
+# With boolean values
+
+print(True>False)
+print(True>=False)
+print(True<False)
+print(True<=False)
+
+print()
+#extras
+print(True<2)
+# print(False<'xyz') can't compare bool and str
+# print(10>'python') can't compare int value with str
+
+#chaining of relational operator
+print(10<20<30<50)
+print(10<20<30<70<40>50)
+
+

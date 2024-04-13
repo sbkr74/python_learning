79f2eac1d20a2c450eb28d5a211c80d1e900b0f3
sbkr74 <sbkr74@gmail.com>
2024-04-13 11:24:59 +0530

    1.2.3.4.Logical_Op.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.4.Logical_Op.py
@@ -0,0 +1,31 @@
+#Logical operator consists of and, or, not
+
+#With boolean values
+print(True and False)
+print(True or False)
+print(not False)    
+
+print()
+# With int value
+print(0 and 1)
+print(0 or 1)
+print(10 and 30)
+print(30 and 10)
+print(10 or 11)
+print(11 or 10)
+print(not 0)
+print(not 5)
+
+print()
+#with string value
+print('python' and 'program')
+print('' and 'python')
+print('python' and '')
+print('python' or 'program')
+print('program' and 'python')
+print('program' or 'python')
+print('python' or '')
+print('' or 'program')
+print(not "")
+print(not "python")
+

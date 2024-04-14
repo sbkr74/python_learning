af230d2d1e023b342681b823efe8015b21c92a63
sbkr74 <sbkr74@gmail.com>
2024-04-14 23:47:57 +0530

    1.2.3.5.bitwise_Op.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.5.bitwise_Op.py
@@ -0,0 +1,10 @@
+# Bitwise operator(&,|,^,~,<<,>>)
+# Bitwise operator are applicable for only int and bool types
+
+print(4&5)
+print(4|5)
+print(4^5)
+
+#complement operator
+print(~5)
+print(~-12)

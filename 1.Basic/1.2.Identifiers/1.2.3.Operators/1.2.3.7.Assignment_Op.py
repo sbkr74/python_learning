a4f7524f015ac665dbc1c2e2c855cf632e88e0f3
sbkr74 <sbkr74@gmal.com>
2024-04-15 22:13:27 +0530

    1.2.3.7.Assignment_Op.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.7.Assignment_Op.py
@@ -0,0 +1,22 @@
+# assignment operator is assign values to variable.
+# only '= 'comes under assignment operator
+x = 10
+print(x)
+# assignment operator + some other operator to form compound assignment operator.
+x+=10
+print(x)
+
+# following are the list of all possible compound assignment operator in python.
+# +=
+# -=
+# *=
+# /=
+# %=
+# //=
+# **=
+# &=
+# |=
+# ^=
+# >>=
+# <<=
+

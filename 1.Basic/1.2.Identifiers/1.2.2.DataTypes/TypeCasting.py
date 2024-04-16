5f03d4877c314f5538963863fe15b6e978168d42
sbkr74 <sbkr74@gmal.com>
2024-04-16 23:38:31 +0530

    TypeCasting.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.2.DataTypes/TypeCasting.py
@@ -0,0 +1,10 @@
+# there are various typecasting function
+# int()
+# float()
+# complex()
+# bool()
+# str()
+print(int(123.66))
+# print(int(3+5j)) ->can't convert complex to int
+print(int(True))
+print(int("100"))

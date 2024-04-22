06a66a5b8c2172fe5c6ad7878476288be1060b1e
sbkr74 <sbkr74@gmal.com>
2024-04-22 20:34:12 +0530

    1.2.3.9.Special_Op.py file updated2.1

--- 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.9.Special_Op.py
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/1.2.3.9.Special_Op.py
@@ -8,7 +8,11 @@ c =11
 d =10
 e= d+1
 print(c is e)
+print(c is not d)
 
+f1 = [1,3,4,6,7,9,4,6,8]
+f2 = [2,5,6,8,9]
+print(f1 is not f2) 
 # 2. Membership Opertor
 # in and not in
 x = "Python programming from Basic to advance."
@@ -18,3 +22,4 @@ print("to" in x)
 y = [1,3,4,6,7,9,4,6,8]
 print(4 in y)
 print(2 in y)
+print(2 not in y)

37a7f1e4bde8f87a9d5af834806b63fc77c7c7ff
sbkr74 <sbkr74@gmal.com>
2024-04-22 20:53:09 +0530

    Operator_precedence.py file added

---
+++ 1.Basic/1.2.Identifiers/1.2.3.Operators/Operator_precedence.py
@@ -0,0 +1,22 @@
+# 1.() -> Parenthesis
+# 2.** -> Exponential
+# 3.~,- -> Bitwise Complement Operator, Uniary Minus Operator
+# 4.*,/,%,// -> Multiplication, Division, Modulo, Floor Division
+# 5.+,- -> Addition, Substraction
+# 6.<<,>> -> Left and Right Shift
+# 7. & -> Bitwise And
+# 8. ^ -> Bitwise XOR
+# 9. | -> Bitwise OR
+# 10.>,>=,<,<=,==,!= -> Relational OR Comparison Operators
+# 11.=,+=,-=,*=,... -> Assignment Operators
+# 12. is, is not -> Identity Operators
+# 13. in, not in -> Membership Operators
+# 14. not -> Logical not
+# 15. and -> Logical and
+# 16. or -> Logical or    
+a = 30
+b = 20
+c = 10
+d = 5
+print((a+b)*c/d)
+print(3/2*4+3+(10/5)**3-2)

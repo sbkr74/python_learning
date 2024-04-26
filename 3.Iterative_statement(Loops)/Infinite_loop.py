2077aab373c75b3ccb3ad7d5d46daddf6c4665cb
sbkr74 <sbkr74@gmail.com>
2024-04-26 12:16:00 +0530

    Infinite_loop.py file added

---
+++ 3.Iterative_statement(Loops)/Infinite_loop.py
@@ -0,0 +1,10 @@
+i = 0
+while True:
+    i=i+1
+    print()
+
+#========================================
+# it is highly recommended to delete the the variable if it is no longer required.
+# so that corresponding object is eligible for garbage collection.
+# del i
+# print(i)

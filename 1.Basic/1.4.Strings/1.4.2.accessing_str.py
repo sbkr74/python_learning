dfc45768c164af08dcf2766f8bb142f7a0efd63f
sbkr74 <sbkr74@gmail.com>
2024-04-29 22:10:40 +0530

    accessing char using slice operator

--- 1.Basic/1.4.Strings/1.4.2.accessing_str.py
+++ 1.Basic/1.4.Strings/1.4.2.accessing_str.py
@@ -6,5 +6,16 @@ print(str[-1])
 s = input("Enter a String:")
 i=0
 for x in s:
-    print("character at +ve index {} and -ve index {} is".format(i,i-len(s),x))
+    print("character at +ve index {} and -ve index {} is {}".format(i,i-len(s),x))
     i+=1
+
+# accessing characters using slice operator.
+# syntax: s[beginindex,endindex,steps]
+y = s
+print(y[1:11:2])
+print(s[:7])
+print(s[7:])
+print(s[::])
+print(s[:])
+# reverse string
+print(s[::-1])

cc28346cc784057648cb871e5fdc17d5d6c16c26
sbkr74 <sbkr74@gmail.com>
2024-04-29 21:43:38 +0530

    1.4.1.strings.py file added

---
+++ 1.Basic/1.4.Strings/1.4.1.strings.py
@@ -0,0 +1,12 @@
+# in python either it is single character or group of character(words,sentence).
+# it is treated as string
+# it can be within single quotes,double quotes or triple quoutes
+s = 'a'
+s1 = "this is a example string"
+s2 = '''example of 
+multi-line string'''
+
+print(s)
+print(type(s))
+print(s1)
+print(s2)

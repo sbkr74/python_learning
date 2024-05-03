8efed87e822b8ef1c22bebd0c6eaa581dc16b4ea
sbkr74 <sbkr74@gmail.com>
2024-05-04 00:03:48 +0530

    1.3.4.bytearray().py file updated2.0

--- 1.Basic/1.3.Built-in-functions/B/1.3.5.bytearray().py
+++ 1.Basic/1.3.Built-in-functions/B/1.3.5.bytearray().py
@@ -1,2 +1,16 @@
 # creating byte array
 # uptill I know it is like bytes() but it is mutuable in nature
+s = ['python', 'program']
+asc = []
+for i in s:
+    for j in i:
+        asc.append(ord(j))
+print(asc)
+b = bytearray(asc)
+print(b)
+
+b.append(109)
+b.append(105)
+b.append(110)
+b.append(103)
+print(b)

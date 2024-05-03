54ec6171a9523f4c4713bd89004515c89f57b370
sbkr74 <sbkr74@gmail.com>
2024-05-03 23:54:02 +0530

    in file 1.2.2.13 line 5 modified(added 3 values)

--- 1.Basic/1.2.Identifiers/1.2.2.DataTypes/1.2.2.12.bytes_dtype.py
+++ 1.Basic/1.2.Identifiers/1.2.2.DataTypes/1.2.2.12.bytes_dtype.py
@@ -2,7 +2,7 @@
 # it takes only decimal value float value can't be converted to bytes.
 # and values range should be from 0 to 255.
 # once created byte data type value it can't be changed.
-a =[100,187,21,255,250,9]
+a =[100,187,21,255,250,9,112,121,116,104]
 # a = [11.1,12.9,111.77]
 b = bytes(a)
 print(b)

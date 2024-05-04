2650e83caf65dffadb37dc2c275af119d853478e
sbkr74 <sbkr74@gmail.com>
2024-05-04 12:30:20 +0530

    unboundLocalError.py file updated2.0 with comments

--- Errors/unboundLocalError.py
+++ Errors/unboundLocalError.py
@@ -1,3 +1,5 @@
+# Raised when a reference is made to a local variable in a function or method, but no value has been bound to that variable.
+# it will raise an UnboundLocalError because the variable y is referenced before it's assigned a value within the function 
 def fun():
     print("Value of Y:",y)
     y = 50

a49853ada70cde396350fc55440c78db823fa09a
sbkr74 <sbkr74@gmail.com>
2024-05-07 22:59:54 +0530

    1.4.3.string_len().py file updated2.0

--- 1.Basic/1.4.Strings/1.4.3.string_len().py
+++ 1.Basic/1.4.Strings/1.4.3.string_len().py
@@ -1,9 +1,9 @@
 # to find length of string
 s = "python programming"
 print(len(s))
-s1 =s[0:6]
+s1 ="python"
 print(s1)
-s2 = s[7:19]
+s2 = "programming"
 print(s2)
 print(len(s2)>len(s1))
 s3 = s1+s2

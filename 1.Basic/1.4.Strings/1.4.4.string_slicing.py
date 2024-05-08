325bf8bc17f048c19f39934626d705f42002dcd0
sbkr74 <sbkr74@gmail.com>
2024-05-08 23:04:08 +0530

    1.4.4.string_slicing.py updated2.0 using character skip

--- 1.Basic/1.4.Strings/1.4.4.string_slicing.py
+++ 1.Basic/1.4.Strings/1.4.4.string_slicing.py
@@ -5,3 +5,12 @@ s2 = s[7:19]
 print(s2)
 s3 = s1+s2
 print(s3)
+
+#slicing using first index onlt
+print(s[7:])
+print(s[-11:])
+
+# Skipping character while splitting Python strings
+language = 'Python'
+pto = language[0:6:2] # 
+print(pto) # pto

2942dee2c559e40b1ed2a3884da9d76c77ea9842
sbkr74 <sbkr74@gmail.com>
2024-05-08 23:07:23 +0530

    1.4.1.strings.py updated2.3(escape sequence

--- 1.Basic/1.4.Strings/1.4.1.strings.py
+++ 1.Basic/1.4.Strings/1.4.1.strings.py
@@ -32,3 +32,13 @@ print(c) # t
 print(d) # h
 print(e) # o
 print(f) # n
+
+# Escape sequence
+print('I hope every one enjoying the python challenge.\nDo you ?') # line break
+print('Days\tTopics\tExercises')
+print('Day 1\t3\t5')
+print('Day 2\t3\t5')
+print('Day 3\t3\t5')
+print('Day 4\t3\t5')
+print('This is a back slash  symbol (\\)') # To write a back slash
+print('In every programming language it starts with \"Hello, World!\"')

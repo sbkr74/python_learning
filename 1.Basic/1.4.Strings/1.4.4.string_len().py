b1e17c82a07a6c3ce4ff9ab41a62bfb6a5a56ee3
Shubham Biruly <45933577+sbkr74@users.noreply.github.com>
2024-05-09 21:28:58 +0530

    Update2.5 and rename 1.4.3.string_len().py to 1.4.4.string_len().py

---
+++ 1.Basic/1.4.Strings/1.4.4.string_len().py
@@ -0,0 +1,8 @@
+# to find length of string
+s = "python programming"
+s1 ="python"
+s2 = "programming"
+s3 = s1+s2
+print(len(s))             #python programming
+print(len(s3))            #19
+print(len(s2)>len(s1))    #True

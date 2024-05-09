14357834c5d0c768d17b99523f833323f1b9a5b0
sbkr74 <sbkr74@gmail.com>
2024-05-09 21:25:11 +0530

    1.4.1.strings.py file updated2.4(removed len())

--- 1.Basic/1.4.Strings/1.4.1.strings.py
+++ 1.Basic/1.4.Strings/1.4.1.strings.py
@@ -17,11 +17,6 @@ space = ' '
 full_name = first_name  +  space + last_name
 print(full_name)
 
-# Checking length of a string using len() builtin function
-print(len(first_name))  
-print(len(last_name))   
-print(len(first_name) > len(last_name)) 
-print(len(full_name)) 
 
 #### Unpacking characters 
 language = 'Python'
@@ -37,8 +32,8 @@ print(f) # n
 print('I hope every one enjoying the python challenge.\nDo you ?') # line break
 print('Days\tTopics\tExercises')
 print('Day 1\t3\t5')
-print('Day 2\t3\t5')
-print('Day 3\t3\t5')
-print('Day 4\t3\t5')
+print('Day 2\t2\t5')
+print('Day 3\t3\t8')
+print('Day 4\t4\t20')
 print('This is a back slash  symbol (\\)') # To write a back slash
 print('In every programming language it starts with \"Hello, World!\"')

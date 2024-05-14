c219d59ce0da406a3049d66a5125036159869446
sbkr74 <sbkr74@gmail.com>
2024-05-14 21:43:23 +0530

    1.3.2.divmod.py file added

---
+++ 1.Basic/1.3.Built-in-functions/D/1.3.2.divmod().py
@@ -0,0 +1,10 @@
+'''
+Take two (non-complex) numbers as arguments and return a pair of numbers consisting of their quotient and remainder when using 
+integer division. With mixed operand types, the rules for binary arithmetic operators apply. 
+For integers, the result is the same as (a // b, a % b). For floating point numbers the result is (q, a % b), 
+where q is usually math.floor(a / b) but may be 1 less than that. In any case q * b + a % b is very close to a, 
+if a % b is non-zero it has the same sign as b, and 0 <= abs(a % b) < abs(b).
+'''
+a = 2
+b = 5
+print(divmod(b,a))

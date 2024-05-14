f096c3350e931e1eb453a62b8a16676060aee3e4
sbkr74 <sbkr74@gmail.com>
2024-05-14 11:50:53 +0530

    1.3.5.classmethod().py file added

---
+++ 1.Basic/1.3.Built-in-functions/C/1.3.5.classmethod().py
@@ -0,0 +1,22 @@
+'''
+It's commonly used to define methods that act on the class itself or on class-level data.
+The primary use cases for classmethod() include:
+    1.) Alternative Constructors: Class methods can be used as alternative constructors. 
+        For example, a class method can parse a string and create an instance of the class based on the parsed data.
+    2.) Factory Methods: They can act as factory methods that return instances of the class based on certain conditions.
+    3.) Accessing Class Variables: They are useful for accessing or modifying class variables that are 
+        shared among all instances of the class.
+'''
+class MyClass:
+    class_variable = 10
+
+    # def __init__(self, x):
+    #     self.x = x
+
+    @classmethod
+    def class_method(cls):
+        print("This is a class method")
+        print("Class variable:", cls.class_variable)
+
+# Call the class method without creating an instance
+MyClass.class_method()

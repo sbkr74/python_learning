d7436fa2d8e71351ef8ece3e129222f6c84e5301
sbkr74 <sbkr74@gmail.com>
2024-05-15 21:13:42 +0530

    1.4.15.string_swapcase().py file added

---
+++ 1.Basic/1.4.Strings/1.4.15.string_swapcase().py
@@ -0,0 +1,8 @@
+# swapcase(): It inverts the text letter by letter
+string = 'python programming from Basic to Advance'
+print(string.swapcase())
+
+# dynamic inputs
+usr_in = input("Enter a text: ")
+# swapping the case
+print(usr_in.swapcase())

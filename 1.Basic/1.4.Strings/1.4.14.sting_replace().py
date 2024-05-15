ba0a08e46f7adaf87729653b55097a0b0756be20
sbkr74 <sbkr74@gmail.com>
2024-05-15 21:06:22 +0530

    1.4.14.string_replace().py file updated2.0(user_input)

--- 1.Basic/1.4.Strings/1.4.14.sting_replace().py
+++ 1.Basic/1.4.Strings/1.4.14.sting_replace().py
@@ -1,3 +1,8 @@
 # replace() is used to replace the substring inside the string 
 string = 'python programming from Basic to Advance'
 print(string.replace('programming','coding'))
+
+# user input ones
+user_input = input("Enter your text: ")
+sub , rep = map(str,input().split())
+print(user_input.replace(sub,rep))

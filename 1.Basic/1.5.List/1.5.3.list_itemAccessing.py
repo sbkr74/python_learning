db9b747e154e46acf5cea01268251e069d8f8482
sbkr74 <sbkr74@gmail.com>
2024-05-16 21:12:46 +0530

    1.5.3.list_itemAccesing.py file added

---
+++ 1.Basic/1.5.List/1.5.3.list_itemAccessing.py
@@ -0,0 +1,14 @@
+# Progamming language as List of items
+items = ['Python','Objective C', 'C++','Bash Script','Java Core','Ruby','Scala','JavaScript']
+
+# accessing item in list
+first_item = items[0]
+last_item = items[-1]
+third_item = items[2]
+last_sec_item = items[-2]
+
+# Printing results
+print("First element in items list:",first_item)
+print("last element in items list:",last_item)
+print("Third element in items list:",third_item)
+print("Second Last element in items list:",last_sec_item)

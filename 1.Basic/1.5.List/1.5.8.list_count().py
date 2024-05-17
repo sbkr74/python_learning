304d7339201cf98a8c42d95b44be2d686e105a23
sbkr74 <sbkr74@gmail.com>
2024-05-17 21:16:09 +0530

    1.5.8.list_count().py file added

---
+++ 1.Basic/1.5.List/1.5.8.list_count().py
@@ -0,0 +1,6 @@
+# count(): It is used to count the occurence of an item in list.
+items = ['Python','Objective C', 'C++','Bash Script','Java Core','Ruby','Scala','JavaScript']
+print(items.count('Scala'))     # 1 
+print(items.count('C'))         # 0 (as it is not present in list)
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.count(24))           # 3

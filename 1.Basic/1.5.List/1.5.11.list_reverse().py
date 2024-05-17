c56b484cf00a309c45945f879aaf79a3de0f3930
sbkr74 <sbkr74@gmail.com>
2024-05-17 21:49:32 +0530

    1.5.11.list_reverse().py file added

---
+++ 1.Basic/1.5.List/1.5.11.list_reverse().py
@@ -0,0 +1,6 @@
+items = ['Python','Objective C', 'C++','Bash Script','Java Core','Ruby','Scala','JavaScript']
+items.reverse()
+print(items)    # ['JavaScript', 'Scala', 'Ruby', 'Java Core', 'Bash Script', 'C++', 'Objective C', 'Python']
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.reverse()
+print(ages)     # [24, 25, 24, 26, 25, 24, 19, 22]

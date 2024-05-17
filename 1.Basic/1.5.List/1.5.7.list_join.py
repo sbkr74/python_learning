49c15e8dc39c83caeb9bef5d5e16c650d7bbefa5
sbkr74 <sbkr74@gmail.com>
2024-05-17 21:50:55 +0530

    1.5.7.list_join.py file updated2.0 (join using extend() function)

--- 1.Basic/1.5.List/1.5.7.list_join.py
+++ 1.Basic/1.5.List/1.5.7.list_join.py
@@ -9,3 +9,13 @@ lang = ['Python','Objective C', 'C++','Bash Script','Java Core','Scala']
 web_technologies = ['HTML', 'CSS', 'JavaScript','React']
 development = lang + web_technologies
 print(development)
+
+
+# join with extend
+num1 = [0, 1, 2, 3]
+num2= [4, 5,6]
+num1.extend(num2)
+print('Numbers:', num1)
+negative_numbers = [-5,-4,-3,-2,-1]
+positive_numbers = [1, 2, 3,4,5]
+zero = [0]

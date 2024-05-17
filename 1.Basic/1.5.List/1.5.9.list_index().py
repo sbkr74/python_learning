99b8c47ff1bc81ee2b19016f1ec229ad7ef867f7
sbkr74 <sbkr74@gmail.com>
2024-05-17 21:21:01 +0530

    1.5.9.list_index().py file added

---
+++ 1.Basic/1.5.List/1.5.9.list_index().py
@@ -0,0 +1,5 @@
+# index(): It is used to find the postion of item in list
+items = ['Python','Objective C', 'C++','Bash Script','Java Core','Ruby','Scala','JavaScript']
+print(items.index('Scala'))     # 6 
+ages = [22, 19, 17,24, 25, 26, 24, 25, 24]
+print(ages.index(24))           # 3

68e27d2bca734c2de0470f6dcfa45fbc9a909b53
sbkr74 <sbkr74@gmail.com>
2024-05-17 21:10:29 +0530

    1.5.7.list_join.py file added

---
+++ 1.Basic/1.5.List/1.5.7.list_join.py
@@ -0,0 +1,11 @@
+# join
+positive_numbers = [1, 2, 3,4,5]
+zero = [0]
+negative_numbers = [-5,-4,-3,-2,-1]
+integers = negative_numbers + zero + positive_numbers
+print(integers)
+
+lang = ['Python','Objective C', 'C++','Bash Script','Java Core','Scala']
+web_technologies = ['HTML', 'CSS', 'JavaScript','React']
+development = lang + web_technologies
+print(development)

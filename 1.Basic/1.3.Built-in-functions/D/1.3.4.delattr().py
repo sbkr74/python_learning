8c6e6f75be71dfada1d784a07eadf375d07cc465
sbkr74 <sbkr74@gmail.com>
2024-05-17 21:39:02 +0530

    1.3.4.delattr().py file updated2.3 (with some more examples)

--- 1.Basic/1.3.Built-in-functions/D/1.3.4.delattr().py
+++ 1.Basic/1.3.Built-in-functions/D/1.3.4.delattr().py
@@ -51,6 +51,35 @@ user_decision = input("Enter the choice: ")
 if user_decision == 'remove_model':
     delattr(car, 'model')
 
+class MyClass:
+    def __init__(self, x):
+        self.x = x
+
+obj = MyClass(5)
+
+# Dynamically delete an attribute based on user input
+attribute_name = input("Enter the attribute name to delete: ")
+if hasattr(obj, attribute_name):
+    delattr(obj, attribute_name)
+
+print(obj.__dict__)
+
+
+class Config:
+    def __init__(self):
+        self.host = "localhost"
+        self.port = 8080
+
+config = Config()
+
+# Dynamically change configuration
+config.host = "example.com"
+config.port = 9090
+
+# Remove outdated configuration
+delattr(config, 'port')
+
+
 # Example 3:
 '''
 Cleaning Up Resources: In some cases, attributes may represent resources that need to be cleaned up when no longer needed, 
@@ -67,4 +96,82 @@ class FileHandler:
 file_handler = FileHandler("example.txt")
 # ... do something with file_handler.file ...
 delattr(file_handler, 'file')  # Close file handle when no longer needed
+
+# Example 4: Deleting a method from a class:
+'''
+
+'''
+class MyClass:
+    def method(self):
+        print("This is a method")
+
+delattr(MyClass, 'method')
+obj = MyClass()
+# Raises AttributeError: 'MyClass' object has no attribute 'method'
+obj.method()
+
+# Example 5: Deleting a property from a class:
+'''
+'''
+class MyClass:
+    def __init__(self):
+        self._value = None
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
+
+delattr(MyClass, 'value')
+obj = MyClass()
+# Raises AttributeError: 'MyClass' object has no attribute 'value'
+obj.value = 42
+
+# Example 6: Data Cleanup
+'''
+Data Cleanup: In data processing tasks, you might create temporary attributes for intermediate results. 
+Once these attributes are no longer needed, you can use delattr() to clean up the object.
+'''
+
+class DataProcessor:
+    def __init__(self, data):
+        self.data = data
+
+    def process_data(self):
+        # Process data and store intermediate results
+        self.result = self.data * 2
+
+    def cleanup(self):
+        # Remove intermediate results
+        delattr(self, 'result')
+
+processor = DataProcessor(10)
+processor.process_data()
+
+# Do something with intermediate result
+
+# Cleanup to remove intermediate result attribute
+processor.cleanup()
+
+
+# Example 6: Security
+'''
+In some security-sensitive applications, you may need to remove sensitive information from objects to prevent unauthorized access.
+'''
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def logout(self):
+        # Remove password attribute to enhance security
+        delattr(self, 'password')
+
+user = User("example_user", "secure_password")
+
+# Perform logout operation
+user.logout()
 

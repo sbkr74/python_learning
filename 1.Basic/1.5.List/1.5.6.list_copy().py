6e5ef938145c24da916949bd30a4e00d9cec8b37
sbkr74 <sbkr74@gmail.com>
2024-05-17 20:29:20 +0530

    1.5.6.list_copy().py file added

---
+++ 1.Basic/1.5.List/1.5.6.list_copy().py
@@ -0,0 +1,11 @@
+# copy(): It is to copy the list
+items = ['Python','Objective C', 'C++','Bash Script','Java Core','Ruby','Scala','JavaScript']
+copied_items = items.copy()
+var = items
+items.pop()
+print(copied_items)
+print(var)
+
+'''
+NOTE: There is difference between copy() and assigning list to another variable as seen in above example. 
+'''

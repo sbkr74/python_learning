ac8bde5ff26bebc12db9bd998641b3fc3bf1aaa1
sbkr74 <sbkr74@gmail.com>
2024-05-18 21:18:31 +0530

    list_check.py file added

---
+++ 1.Basic/1.5.List/list_check.py
@@ -0,0 +1,6 @@
+# checking
+lang = ['Python','Objective C', 'C++','Bash Script','Java Core','Scala']
+script_lang = 'Bash Script' in lang
+web_technologies = 'JavaScript' in lang
+print(script_lang)
+print(web_technologies)

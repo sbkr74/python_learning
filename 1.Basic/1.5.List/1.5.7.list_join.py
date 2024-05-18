886b0ea5c680521fe410a7faf18e7d08b7b16567
sbkr74 <sbkr74@gmail.com>
2024-05-18 20:44:26 +0530

    1.5.7.list_join.py file updated2.1(extend example)

--- 1.Basic/1.5.List/1.5.7.list_join.py
+++ 1.Basic/1.5.List/1.5.7.list_join.py
@@ -19,3 +19,6 @@ print('Numbers:', num1)
 negative_numbers = [-5,-4,-3,-2,-1]
 positive_numbers = [1, 2, 3,4,5]
 zero = [0]
+negative_numbers.extend(zero)
+negative_numbers.extend(positive_numbers)
+print('Integers:', negative_numbers)

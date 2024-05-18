7549c946fd4f1666f9c05bf1f375b15469375ef6
sbkr74 <sbkr74@gmail.com>
2024-05-18 23:11:40 +0530

    1.3.2.exec().py file updated2.3(with scenarios)

--- 1.Basic/1.3.Built-in-functions/E/1.3.2.exec().py
+++ 1.Basic/1.3.Built-in-functions/E/1.3.2.exec().py
@@ -35,5 +35,40 @@ exec('c = a + b', {}, local_vars)
 print(local_vars['c'])  # Output: 3
 print(type(local_vars))
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Scenarios to use exec
+'''
+1. Dynamic Code Execution:
+When the code to be executed is dynamically generated at runtime, exec() can be used. 
+This is common in scenarios where the exact nature of the code cannot be determined until runtime.
+'''
+# code_snippet = input("Enter code to execute: ")
+# exec(code_snippet)
+
+'''
+2. Metaprogramming:
+In metaprogramming, where the program writes or manipulates code, exec() can be useful. 
+This can be seen in advanced applications such as generating classes or functions dynamically based on input.
+'''
+class_code = """
+class DynamicClass:
+    def __init__(self, value):
+        self.value = value
+    def display(self):
+        print(f'Value: {self.value}')
+"""
+exec(class_code)
+instance = DynamicClass(10)
+instance.display()  # Output: Value: 10
+
+'''
+3. Loading Configuration or Code from External Sources:
+
+If you need to load and execute code from an external source like a configuration file, exec() can be handy. 
+This is risky and should be done with caution and proper validation of the code.
+'''
+with open('1.Basic/1.3.Built-in-functions/E/1.3.1.eval().py', 'r') as file:
+    config_code = file.read()
+exec(config_code)
 
 

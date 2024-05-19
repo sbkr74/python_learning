94c719a8f48d6c25c0543983e31d34f9e4a50203
sbkr74 <sbkr74@gmail.com>
2024-05-19 21:12:37 +0530

    1.6.5.tuple_modification.py file updated2.0(list_funtions)

--- 1.Basic/1.6.Tuple/1.6.5.tuple_modification.py
+++ 1.Basic/1.6.Tuple/1.6.5.tuple_modification.py
@@ -1,8 +1,22 @@
 # We can change tuples to lists and lists to tuples. Tuple is immutable if we want to modify a tuple we should change it to a list.
 my_tuple = ('Java','Python','SQL','Scala','Spark','HDFS','YARN')
 my_tuple = list(my_tuple)
-my_tuple.append('kubernetes')
-my_tuple[0] = 'Bash'
+my_tuple.append('kubernetes')   # append()
+my_tuple[0] = 'Bash'            # index
+my_tuple.insert(5,'Docker')     # insert()
 my_tuple = tuple(my_tuple)
 print(my_tuple)
 print(type(my_tuple))
+
+my_tuple = list(my_tuple)
+my_tuple.remove('YARN')         # remove()
+my_tuple.remove(my_tuple[1])    # remove() index
+my_tuple.pop()                  # pop()
+my_tuple.pop(2)                 # pop() index
+my_tuple = tuple(my_tuple)
+print(my_tuple)
+print(type(my_tuple))
+
+# Deleting Tuples
+# It is not possible to remove a single item in a tuple but it is possible to delete the tuple itself using del.
+del my_tuple

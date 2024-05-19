c3eec00cd4ea3ef05731baa44b6e7ca8430ddf83
sbkr74 <sbkr74@gmail.com>
2024-05-19 21:20:16 +0530

    1.6.6.tuple_count().py file added

---
+++ 1.Basic/1.6.Tuple/1.6.6.tuple_count().py
@@ -0,0 +1,5 @@
+my_tuple = ('Java','Python','SQL','Scala','Spark','HDFS','YARN','Python')
+ages = (22, 19, 24, 25, 26, 24, 25, 24)
+print(my_tuple.count('Python'))     # 2
+print(my_tuple.count('C++'))        # 0
+print(ages.count(24))               # 3

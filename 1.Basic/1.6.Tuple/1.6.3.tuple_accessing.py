f62216c903ad181d03b6b5620187c303bf8b30ce
sbkr74 <sbkr74@gmail.com>
2024-05-19 20:42:37 +0530

    1.6.3.tuple_accessing.py file added

---
+++ 1.Basic/1.6.Tuple/1.6.3.tuple_accessing.py
@@ -0,0 +1,6 @@
+# accessing tuple items using index position
+my_tuple = ('Java','Python','SQL','Scala')
+print(my_tuple[1])                  # Python
+print(my_tuple[-1])                 # Scala
+print(my_tuple[len(my_tuple)-1])    # Scala
+

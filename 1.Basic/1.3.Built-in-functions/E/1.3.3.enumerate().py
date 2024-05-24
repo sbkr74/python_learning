33d8eb660ca69744b226a5ce92143c31e4353257
sbkr74 <sbkr74@gmail.com>
2024-05-24 20:31:52 +0530

    1.3.3.enumerate().py file added

---
+++ 1.Basic/1.3.Built-in-functions/E/1.3.3.enumerate().py
@@ -0,0 +1,14 @@
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+print(list(enumerate(seasons)))
+
+print(list(enumerate(seasons, start=1)))
+x = list(enumerate(seasons,start=2))
+print(x)
+
+def enum(iterable, start=0):
+    n = start
+    for elem in iterable:
+        yield n, elem
+        n += 1
+enum(seasons,1)
+print(list(enum(seasons,1)))

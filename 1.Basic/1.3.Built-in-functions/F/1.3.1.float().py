71f9077ffd808fd3095545ff14e0e268f9a46e69
sbkr74 <sbkr74@gmail.com>
2024-05-25 23:18:06 +0530

    1.3.1.float().py file added

---
+++ 1.Basic/1.3.Built-in-functions/F/1.3.1.float().py
@@ -0,0 +1,6 @@
+# Return a floating point number constructed from a number or string x.
+# used for converting string or integer to floating point value. 
+x = 10
+print(float(x))
+s = '12'
+print(float(s))

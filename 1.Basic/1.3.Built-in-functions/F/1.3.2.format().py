d5b2770234c6007e724fef6e2de4ed7f5e0ce92c
sbkr74 <sbkr74@gmail.com>
2024-05-26 22:19:17 +0530

    1.3.2.format().py file added

---
+++ 1.Basic/1.3.Built-in-functions/F/1.3.2.format().py
@@ -0,0 +1,12 @@
+# format() : it is used to define how individual values are represented
+'''
+
+The format() method in Python is used for string formatting, allowing you to insert variables or expressions 
+into strings in a readable and organized manner. 
+This method provides a more flexible way of formatting strings compared to the older % formatting style.
+'''
+# Example 1: Simple Replacement
+name = "Simba"
+age = 21
+formatted_string = "My name is {} and I am {} years old.".format(name, age)
+print(formatted_string)

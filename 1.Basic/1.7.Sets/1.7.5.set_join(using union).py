d5cb697ee52d2037c169408f1be9d21ae58cfff8
sbkr74 <sbkr74@gmail.com>
2024-05-27 15:12:27 +0530

    updated2.0 with intersection()

--- 1.Basic/1.7.Sets/1.7.5.set_join(using union).py
+++ 1.Basic/1.7.Sets/1.7.5.set_join(using union).py
@@ -12,3 +12,16 @@ st1 = {'item1', 'item2', 'item3', 'item4'}
 st2 = {'item5', 'item6', 'item7', 'item8'}
 st1.update(st2)
 print(st1)
+
+########################################################################
+# intersection between two sets
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+even_numbers = {0, 2, 4, 6, 8, 10}
+intersect = whole_numbers.intersection(even_numbers)
+print(intersect)
+
+python = {'p', 'y', 't', 'h', 'o','n'}
+anaconda = {'a','n','a','c','o','n','d','a'}
+intersect = python.intersection(anaconda) 
+print(intersect) 
+

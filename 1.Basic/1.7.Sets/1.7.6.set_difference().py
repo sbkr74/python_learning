7a908cf4851fc7427bc37ae3442e967c66894986
sbkr74 <sbkr74@gmail.com>
2024-05-27 15:19:25 +0530

    updated2.0 with symmetric_difference()

--- 1.Basic/1.7.Sets/1.7.6.set_difference().py
+++ 1.Basic/1.7.Sets/1.7.6.set_difference().py
@@ -7,5 +7,20 @@ dragon = {'d', 'r', 'a', 'g', 'o','n'}
 print(python.difference(dragon))     # {'p', 'y', 't'}  - the result is unordered (characteristic of sets)
 print(dragon.difference(python))     # {'d', 'r', 'a', 'g'}
 
+########################################################
+# Finding Symmetric Difference Between Two Sets
+'''
+It returns the the symmetric difference between two sets. 
+It means that it returns a set that contains all items from both sets, 
+except items that are present in both sets, mathematically: (A\B) ∪ (B\A)
+'''
+
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+some_numbers = {1, 2, 3, 4, 5}
+print(whole_numbers.symmetric_difference(some_numbers)) # {0, 6, 7, 8, 9, 10}
+
+python = {'p', 'y', 't', 'h', 'o','n'}
+dragon = {'d', 'r', 'a', 'g', 'o','n'}
+print(python.symmetric_difference(dragon))  # {'r', 't', 'p', 'y', 'g', 'a', 'd', 'h'}
 
    

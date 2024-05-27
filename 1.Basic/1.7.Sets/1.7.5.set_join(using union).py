1fb2a5653a4271720544c782c41efc6eb11024e0
sbkr74 <sbkr74@gmail.com>
2024-05-27 15:03:50 +0530

    1.7.5.set_join(using union).py file added

---
+++ 1.Basic/1.7.Sets/1.7.5.set_join(using union).py
@@ -0,0 +1,14 @@
+# Joining Sets
+'''We can join two sets using the union() or update() method.'''
+
+# union()
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item5', 'item6', 'item7', 'item8'}
+st3 = st1.union(st2)
+print(st3)
+
+# update()
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item5', 'item6', 'item7', 'item8'}
+st1.update(st2)
+print(st1)

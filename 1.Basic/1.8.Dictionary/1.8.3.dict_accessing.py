7fb4e1362754a9429ffb677276e2a08773bde106
sbkr74 <sbkr74@gmail.com>
2024-05-29 12:20:35 +0530

    1.8.3.dict_accessing.py file added

---
+++ 1.Basic/1.8.Dictionary/1.8.3.dict_accessing.py
@@ -0,0 +1,26 @@
+person = {
+    'first_name':'xyz',
+    'last_name':'abc',
+    'age':60,
+    'country':'India',
+    'skills':['JavaScript', 'React', 'Python', 'MongoDB', 'Node'],
+    'address':{
+        'street':'Somewhere in India',
+        'zipcode':'888888'
+    }
+    }
+
+# accessing items from dictionary
+# we can acess item using its key.
+print(person['age'])
+print(person['country'])
+print(person['skills'][2])
+print(person['address']['zipcode'])
+# print(person['city'])                           # keyError
+
+# if key is not present then we will get key error. if we are accesing using keys
+# Alternative is get()
+print("\nUsing get():")
+print(person.get('age'))
+print(person.get('skills')[2])
+print(person.get('city'))

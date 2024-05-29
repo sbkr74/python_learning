cf1f58d7934c5532c778b5c5d7b8fa43b03bdc61
sbkr74 <sbkr74@gmail.com>
2024-05-29 11:39:47 +0530

    1.8.2.dict_len().py file added

---
+++ 1.Basic/1.8.Dictionary/1.8.2.dict_len().py
@@ -0,0 +1,21 @@
+person = {
+    'first_name':'xyz',
+    'last_name':'abc',
+    'age':60,
+    'country':'India',
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Somewhere in India',
+        'zipcode':'888888'
+    }
+    }
+print(len(person))      # 6
+
+job ={'Destination':['Associate','Junior Associate','Senior Associate'],
+      'location':{
+          'city':'Cyber City',
+          'area':'Techpark'
+      },
+      'salary':80
+      }
+print(len(job))         # 3

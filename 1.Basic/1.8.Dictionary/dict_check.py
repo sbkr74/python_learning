666ed3172919286b9519e84ace6212926639449e
sbkr74 <sbkr74@gmail.com>
2024-05-29 13:39:24 +0530

    dict_check.py file added

---
+++ 1.Basic/1.8.Dictionary/dict_check.py
@@ -0,0 +1,19 @@
+person = {
+    'first_name':'xyz',
+    'last_name':'abc',
+    'age':60,
+    'country':'India',
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Somewhere in India',
+        'zipcode':'888888'
+    }
+    }
+
+# check key is present in dictionary
+print('skills' in person)
+print('city' in person)
+
+# check value in dictionary
+print('India' in person.values())
+print(60 in person.values())

c0fe6cfe5ceb7604a43cc22b7dd7bce2e90e3ee6
sbkr74 <sbkr74@gmail.com>
2024-05-31 21:36:19 +0530

    1.3.1.input().py file added

---
+++ 1.Basic/1.3.Built-in-functions/I/1.3.2.input().py
@@ -0,0 +1,8 @@
+'''
+ input(prompt)
+
+    If the prompt argument is present, it is written to standard output without a trailing newline. 
+    The function then reads a line from input, converts it to a string (stripping a trailing newline), and returns that.
+'''
+s = input("--> ")
+print(s)

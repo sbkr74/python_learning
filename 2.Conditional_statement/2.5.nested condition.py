d57b7af9a209af3d91a9fa7e3077ea2c6eed97ea
sbkr74 <sbkr74@gmail.com>
2024-05-31 20:52:39 +0530

    2.5.nested_condition.py file added

---
+++ 2.Conditional_statement/2.5.nested condition.py
@@ -0,0 +1,10 @@
+a = 0
+if a > 0:
+    if a % 2 == 0:
+        print('A is a positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is a negative number')

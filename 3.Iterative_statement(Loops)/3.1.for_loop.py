a1f2b9fc26257d384ac032b967ed0f03a22d1228
sbkr74 <sbkr74@gmail.com>
2024-06-05 19:25:44 +0530

    3.1.for_loop.py file added

--- 3.Iterative_statement(Loops)/3.1.for_loop.py
+++ 3.Iterative_statement(Loops)/3.1.for_loop.py
@@ -1,2 +1,70 @@
 for x in range(1,5+1):
     print(x)
+
+language = 'Python'
+for letter in language:
+    print(letter,end=' ')
+
+print()
+for i in range(len(language)):
+    print(language[i])
+
+# from List
+numbers = [0, 1, 2, 3, 4, 5]
+for number in numbers: 
+    print(number)
+
+# from Tuple
+numbers = (0, 1, 2, 3, 4, 5)
+for number in numbers:
+    print(number)
+
+# from Set
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+for company in it_companies:
+    print(company)
+
+# from Dictionary
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+for key in person:
+    print(key)
+
+for key, value in person.items():
+    print(key, value)
+
+# Using Break
+numbers = (0,1,2,3,4,5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        break
+
+# Using Continue
+numbers = (0,1,2,3,4,5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        continue
+    print('Next number should be ', number + 1) if number != 5 else print("loop's end") # for short hand conditions need both if and else statements
+print('outside the loop')
+
+# using else
+for number in range(11):
+    print(number)   # prints 0 to 10, not including 11
+else:
+    print('The loop stops at', number)
+
+# use of pass
+for number in range(6):
+    pass

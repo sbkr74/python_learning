702130a6378210734c9b56181e9f90164264ba60
sbkr74 <sbkr74@gmail.com>
2024-06-07 21:49:53 +0530

    5.2.4.fun_para(key-value).py file added

---
+++ 5.Functions/5.2.user-defined/5.2.4.fun_para(key-value).py
@@ -0,0 +1,10 @@
+def print_fullname(firstname, lastname):
+    space = ' '
+    full_name = firstname  + space + lastname
+    print(full_name)
+print_fullname(firstname = 'Shubham', lastname = 'Biruly')
+
+def add_two_numbers (num1, num2):
+    total = num1 + num2
+    print(total)
+add_two_numbers(num2 = 3, num1 = 2) # Order does not matter

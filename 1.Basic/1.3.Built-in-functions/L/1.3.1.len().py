5b555253b6181445b8e3b6a1282a4b4190104718
sbkr74 <sbkr74@gmail.com>
2024-06-13 22:12:21 +0530

    1.3.1.len().py file added

---
+++ 1.Basic/1.3.Built-in-functions/L/1.3.1.len().py
@@ -0,0 +1,10 @@
+# len(): is used to find the length of string and no of items in list,set,tuple,...
+string = "I am Python Developer"
+myList = [1,3,4,'st','ri','ng']
+myTuple = (1,3,'st','ri','ng',3,1)
+mySet = {1,4,'st',7,88}
+
+print(len(string))
+print(len(myList))
+print(len(myTuple))
+print(len(mySet))

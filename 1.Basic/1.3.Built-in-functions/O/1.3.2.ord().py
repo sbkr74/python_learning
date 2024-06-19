089adbe7f19f5213f2b623632b92ae4a979de6bf
sbkr74 <sbkr74@gmail.com>
2024-06-19 23:05:45 +0530

    1.3.2.ord().py file added

---
+++ 1.Basic/1.3.Built-in-functions/O/1.3.2.ord().py
@@ -0,0 +1,8 @@
+''' Given a string representing one Unicode character, return an integer representing the Unicode code point of that character. 
+    For example, ord('a') returns the integer 97 and ord('€') (Euro sign) returns 8364. This is the inverse of chr().'''
+x = ord('a')
+print(x)
+y = chr(8364)
+print(y)
+z = ord('€')
+print(z)

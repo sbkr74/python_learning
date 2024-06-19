2b9e840abbb25c1d54f9690669b25b6e90f8390c
sbkr74 <sbkr74@gmail.com>
2024-06-19 22:45:03 +0530

    1.3.1.next().py file updated2.0(use of default and loop)

--- 1.Basic/1.3.Built-in-functions/N/1.3.1next().py
+++ 1.Basic/1.3.Built-in-functions/N/1.3.1next().py
@@ -7,3 +7,22 @@ print(next(iterator))  # Output: 1
 print(next(iterator))  # Output: 2
 print(next(iterator))  # Output: 3
 
+'''If we call next() again, it will raise StopIteration because the iterator is exhausted'''
+# print(next(iterator))  # This will raise StopIteration
+
+'''We can provide a default value to next() to avoid the StopIteration exception when the iterator is exhausted.'''
+iterator = iter(my_list)
+
+print(next(iterator, 'default'))  # Output: 1
+print(next(iterator, 'default'))  # Output: 2
+print(next(iterator, 'default'))  # Output: 3
+print(next(iterator, 'default'))  # Output: default
+
+'''Using next() in a Loop
+In practice, iterating through an iterator is often done using a loop, which internally uses next().'''
+iterator = iter(my_list)
+
+for item in iterator:
+    print(item)
+'''This loop is equivalent to manually calling next() until a StopIteration exception is raised.'''
+

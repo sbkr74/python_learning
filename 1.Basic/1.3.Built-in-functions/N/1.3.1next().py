06a5a2497c7c7a7d847b6805b1f7151faf5984e0
sbkr74 <sbkr74@gmail.com>
2024-06-19 22:35:48 +0530

    1.3.1.next().py file added

---
+++ 1.Basic/1.3.Built-in-functions/N/1.3.1next().py
@@ -0,0 +1,9 @@
+ # Creating an iterator from a list
+my_list = [1, 2, 3]
+iterator = iter(my_list)
+
+# Using next() to get items from the iterator
+print(next(iterator))  # Output: 1
+print(next(iterator))  # Output: 2
+print(next(iterator))  # Output: 3
+

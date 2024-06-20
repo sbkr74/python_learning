8a9835147e58926509bd7dcf16677c7188d14a46
sbkr74 <sbkr74@gmail.com>
2024-06-20 23:49:05 +0530

    1.3.2.pow().py file added

---
+++ 1.Basic/1.3.Built-in-functions/P/1.3.2.pow().py
@@ -0,0 +1,5 @@
+# pow(base, exp, mod=None)
+'''(computed more efficiently than pow(base, exp) % mod).'''
+print(pow(38,-1,mod=97))
+'''The two-argument form pow(base, exp) is equivalent to using the power operator: base**exp.'''
+print(pow(3,3))

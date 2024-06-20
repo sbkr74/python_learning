98848d8d64722c433dc4eab8977383b4715bf3bc
sbkr74 <sbkr74@gmail.com>
2024-06-20 23:38:26 +0530

    1.3.1.print().py file updated2.0 (file)

--- 1.Basic/1.3.Built-in-functions/P/1.3.1.print().py
+++ 1.Basic/1.3.Built-in-functions/P/1.3.1.print().py
@@ -6,3 +6,11 @@ print("World")
 
 print("Hello", "World", sep=", ", end="!!!")
 print(" How are you?")
+
+
+# Open a file in write mode
+with open("Data/output.txt", "w") as f:
+    print("Hello, World!", file=f)
+# Open a file in append mode
+with open("Data/output.txt", "a") as f:
+    print("Appending this line.", file=f)

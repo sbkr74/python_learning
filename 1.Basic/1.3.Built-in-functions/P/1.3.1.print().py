bd5afc2896db96ec8b7cc5969767b4f8771fa6f7
sbkr74 <sbkr74@gmail.com>
2024-06-20 23:32:35 +0530

    1.3.1.print().py file added

---
+++ 1.Basic/1.3.Built-in-functions/P/1.3.1.print().py
@@ -0,0 +1,8 @@
+print("Hello programmers to Python Basics.")
+print("Hello","programmers","to Python Basics.",sep='\n')
+print("Hello", end=" ")
+print("World")
+
+
+print("Hello", "World", sep=", ", end="!!!")
+print(" How are you?")

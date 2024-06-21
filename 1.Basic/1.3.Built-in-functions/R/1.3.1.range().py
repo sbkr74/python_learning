823949c6b6b3e7209228c90d48cb46a086e5a299
sbkr74 <sbkr74@gmail.com>
2024-06-21 20:37:28 +0530

    1.3.1.range().py file added

---
+++ 1.Basic/1.3.Built-in-functions/R/1.3.1.range().py
@@ -0,0 +1,18 @@
+# range(): It is used to generate immutable sequence of numbers.
+# range(stop)
+seq = list(range(10))
+print(seq)
+
+# range(start,stop)
+seq = list(range(1,11))
+print(seq)
+
+# range(start,stop,step)
+seq = list(range(1,11,2))
+print(seq)
+
+seq = list(range(0,10,2))
+print(seq)
+
+seq = list(range(10,0,-1))
+print(seq)

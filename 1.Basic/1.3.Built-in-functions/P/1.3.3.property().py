6f9f37adbbb7d8a8782f2f055a8b9a50789658f8
sbkr74 <sbkr74@gmail.com>
2024-06-21 20:10:53 +0530

    1.3.3.property().py file updated2.0(age)

--- 1.Basic/1.3.Built-in-functions/P/1.3.3.property().py
+++ 1.Basic/1.3.Built-in-functions/P/1.3.3.property().py
@@ -1,6 +1,7 @@
 class Person:
-    def __init__(self,name):
+    def __init__(self,name,age):
         self._name = name
+        self._age = age
     
     def get_name(self):
         print("Getting Name")
@@ -13,12 +14,23 @@ class Person:
     def del_name(self):
         print("Deleting Name")
         del self._name
+    
+    def get_age(self):
+        return self._age
+    
+    def set_age(self,val):
+        self._age = val
+
 
     name = property(get_name,set_name,del_name,"Name Property")
+    age = property(get_age,set_age,fdel=None,doc="Age property")
 
 # Usage
-p = Person("Alice")
+p = Person("Alice",25)
 print(p.name)
+print(p.age)
 p.name = "Alex"
+p.age = 23
 print(p.name)
+print(p.age)
 del p.name

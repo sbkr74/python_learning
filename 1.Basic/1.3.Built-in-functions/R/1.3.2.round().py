e1fa6b11c9f339d50f83af31e5f76ed885f8ecbf
sbkr74 <sbkr74@gmail.com>
2024-06-21 20:43:51 +0530

    1.3.2.round().py file added

---
+++ 1.Basic/1.3.Built-in-functions/R/1.3.2.round().py
@@ -0,0 +1,9 @@
+# round(number, ndigits=None)
+'''Return number rounded to ndigits precision after the decimal point. 
+If ndigits is omitted or is None, it returns the nearest integer to its input.'''
+pi = 22/7
+print(pi)
+print(round(pi))
+print(round(pi,2))
+print(round(376.87765))
+print(round(376.87765,3))

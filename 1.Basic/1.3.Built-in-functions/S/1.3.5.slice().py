cc2bf710ea2625b042619cabae14343bda951d4e
sbkr74 <sbkr74@gmail.com>
2024-06-24 22:06:35 +0530

    1.3.5.slice().py file updated2.0

--- 1.Basic/1.3.Built-in-functions/S/1.3.5.slice().py
+++ 1.Basic/1.3.Built-in-functions/S/1.3.5.slice().py
@@ -1,6 +1,25 @@
+# string
 string = "Python Programming from basic to advance"
 s = slice(7,19)
-print(string[s])
+print(string[s])                # Output: Programming
+
+# list
 myList = string.split(' ')
 l = slice(1,6,2)
-print(myList[l])
+print(myList[l])                # Output: ['Programming', 'basic', 'advance']
+
+# tuple
+my_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+s = slice(1, 7, 2)
+print(my_tuple[s])              # Output: (1, 3, 5)
+
+
+# Using slice() with indices()
+'''
+The indices() method of the slice object returns a tuple containing the start, stop, 
+and step values as per the length of the sequence.'''
+my_slice = slice(1, 10, 2)
+print(my_slice.indices(20))     # Output: (1, 10, 2)
+
+for i in range(*my_slice.indices(20)):
+    print(i, end=" ")           # Output: 1 3 5 7 9

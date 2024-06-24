2e1f8e78983b3d52d02b90dbae7b4f7f20ff26e7
sbkr74 <sbkr74@gmail.com>
2024-06-24 20:54:31 +0530

    1.3.1.str().py file added

---
+++ 1.Basic/1.3.Built-in-functions/S/1.3.1.str().py
@@ -0,0 +1,9 @@
+# str(): converts object to string.
+string = 'abc123'
+num = 111
+snum = str(num)
+b = bytes('python',encoding='utf-8')
+print(str(string))
+print(type(snum))
+print(b)
+print(str(b,encoding='utf-8'))

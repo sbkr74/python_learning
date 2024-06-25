a852864eae4dbf1682735903b28f65facab3ee13
sbkr74 <sbkr74@gmail.com>
2024-06-25 21:14:04 +0530

    1.3.6.setattr().py file added

---
+++ 1.Basic/1.3.Built-in-functions/S/1.3.6.setattr().py
@@ -0,0 +1,13 @@
+class Person:
+    pass
+
+# objext of class
+obj = Person()
+
+# setting attributes for Person class.
+setattr(obj,'name','Shubham Biruly')
+setattr(obj,'age',25)
+
+# printing
+print(obj.name)     # Shubham Biruly
+print(obj.age)      # 25

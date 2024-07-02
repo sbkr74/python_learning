c50443968ac805a419958aadcb2f7edfb95f5b0f
sbkr74 <sbkr74@gmail.com>
2024-07-02 19:37:25 +0530

    location() used as loc()

--- 6.Modules/user-def-mod/main.py
+++ 6.Modules/user-def-mod/main.py
@@ -1,8 +1,10 @@
 import mymodule as m
+from mymodule import location as loc
 greet =m.greet(m.full_name,"Shubham","Biruly")
 print(greet)
 department = m.depart('IT')
 print(m.full_name("Shubham","Biruly")+" works in "+department)
 salary = m.salary('Junior Associate',m.depart('IT'))
-print(salary)
-print("Associate Salary",m.salary("Associate",m.depart("Dev")))
+print("Initial Salary:",salary)
+print("Associate Salary:",m.salary("Associate",m.depart("Dev")))
+print(loc("Hyderabad"))

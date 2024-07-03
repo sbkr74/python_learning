7737acb1c399ee60be6af808970459516ec7ce65
sbkr74 <sbkr74@gmail.com>
2024-07-03 23:00:54 +0530

    6.4.math_mod.py file updated2.0(examples added)

--- 6.Modules/built-in-mod/6.4.math_mod.py
+++ 6.Modules/built-in-mod/6.4.math_mod.py
@@ -1,6 +1,23 @@
 # So Basically Module is the collection of function,variable and classes
 import math
-print(math.sqrt(16))
-print(math.pi)
 print(dir(math))
+print(math.sqrt(16))
+print(math.pow(2,4))
+print(math.pi)
 print(math.e)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++
+import cmath
+print(cmath.sqrt(-1))
+print(cmath.sqrt(4+9j))
+
+print(cmath.phase(4+9j))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+def decibels(intensity):
+    return 10 * math.log10(intensity)
+
+print(decibels(1000)) 
+print(decibels(1000000)) 

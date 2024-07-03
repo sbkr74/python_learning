ae0bed46b47e425292f64dffa53bf4e63be885e4
sbkr74 <sbkr74@gmail.com>
2024-07-03 23:08:14 +0530

    6.5.random_mod.py file updated2.0(examples added)

--- 6.Modules/built-in-mod/6.5.random_mod.py
+++ 6.Modules/built-in-mod/6.5.random_mod.py
@@ -1,4 +1,15 @@
-from random import random, randint, randrange
+from random import random, randint, randrange, choice
 print(random())
 print(randint(5,20))
 print(randrange(5,20,3))
+
+# Complex Examples
+dice_rolls = [randint(1, 6) for _ in range(2)]
+print(dice_rolls)
+
+# Password generator
+import string
+
+characters = string.ascii_letters + string.digits + string.punctuation
+random_password = ''.join(choice(characters) for _ in range(10))
+print(random_password)

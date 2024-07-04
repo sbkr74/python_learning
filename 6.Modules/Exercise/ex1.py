4eed8a606e1ceb466c04ae4bc15b267a6526951e
sbkr74 <sbkr74@gmail.com>
2024-07-04 20:56:42 +0530

    ex1.py file updated2.0

--- 6.Modules/Exercise/ex1.py
+++ 6.Modules/Exercise/ex1.py
@@ -5,5 +5,12 @@ def random_user_id():
     user_id = ''.join(r.choice(alpha) for _ in range(6))
     return user_id
 
+def user_id_gen_by_user():
+    len_of_id,no_of_id = map(str,input().split())
+    alpha = s.ascii_lowercase + s.digits
+    user_id = ''.join(r.choice(alpha) for _ in range(int(len_of_id)))
+    for _ in range(int(no_of_id)):
+        return user_id
 if __name__ == __name__:
     print(random_user_id())
+    print(user_id_gen_by_user())

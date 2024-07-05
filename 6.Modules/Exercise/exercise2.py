6e535abbcdc6eeeae585ee404718d117ce81c3b8
sbkr74 <sbkr74@gmail.com>
2024-07-05 19:09:23 +0530

    exercise2.py file updated3.0

--- 6.Modules/Exercise/exercise2.py
+++ 6.Modules/Exercise/exercise2.py
@@ -1,17 +1,17 @@
 import random
 import string
-def list_of_hexa_colors():
+def list_of_hexa_colors(num=3):
     s = string.ascii_lowercase
     n = string.digits
     hexa = []
-    for _ in range(3):
+    for _ in range(num):
         hexacode = '#'+''.join(random.choice(s[0:6]+n) for _ in range(6))
         hexa.append(hexacode)
     return hexa
 
-def list_of_rgb_colors():
+def list_of_rgb_colors(n=3):
     rgb = []
-    for _ in range(3):
+    for _ in range(n):
         r = random.randint(0,255)
         g = random.randint(0,255)
         b = random.randint(0,255)
@@ -19,7 +19,13 @@ def list_of_rgb_colors():
         rgb.append(col)
     return rgb
 
+def generate_colors(code,num):
+    if code == 'hexa':
+        return list_of_hexa_colors(num)
+    else:
+        return list_of_rgb_colors(num)
 
 if __name__ == '__main__':
     print(list_of_hexa_colors())
     print(list_of_rgb_colors())
+    print(generate_colors('hexa',5))

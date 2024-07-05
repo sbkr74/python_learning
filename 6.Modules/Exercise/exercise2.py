35b10a0fe0831d0fbd625e0d10f3118fa8fbdd04
sbkr74 <sbkr74@gmail.com>
2024-07-05 19:03:00 +0530

    exercise2.py file updated2.0

--- 6.Modules/Exercise/exercise2.py
+++ 6.Modules/Exercise/exercise2.py
@@ -9,6 +9,17 @@ def list_of_hexa_colors():
         hexa.append(hexacode)
     return hexa
 
+def list_of_rgb_colors():
+    rgb = []
+    for _ in range(3):
+        r = random.randint(0,255)
+        g = random.randint(0,255)
+        b = random.randint(0,255)
+        col = f'rgb({r},{g},{b})'
+        rgb.append(col)
+    return rgb
+
 
 if __name__ == '__main__':
     print(list_of_hexa_colors())
+    print(list_of_rgb_colors())

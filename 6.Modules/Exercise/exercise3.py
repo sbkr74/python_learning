0bdde16e1de5e3c202d6a99abedf6bdd2036d0b9
sbkr74 <sbkr74@gmail.com>
2024-07-08 21:40:10 +0530

    exercise3.py file updated2.0(random7() added)

--- 6.Modules/Exercise/exercise3.py
+++ 6.Modules/Exercise/exercise3.py
@@ -1,6 +1,6 @@
 from random import shuffle as sh
 from random import sample as sa
-
+from random import randint
 # modifying the original list
 def shuffle_list(inList:list):
     sh(inList)
@@ -9,7 +9,15 @@ def shuffle_list(inList:list):
 def sample_list(userList:list):
     return(sa(userList,len(userList)))
 
-    
+def random7():
+    rand7 = []
+    while len(rand7)<7:
+        r = randint(0,9)
+        if r not in rand7:
+            rand7.append(r)
+    return rand7
+
+
 
 if __name__ == '__main__':
     myList = [1,2,3,4,5]
@@ -19,4 +27,6 @@ if __name__ == '__main__':
     print(sample_list(myList))
     print(shuffleList)
 
+    print(random7())
+    
 

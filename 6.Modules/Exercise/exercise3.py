c29d2fa108d5a645d1684212a5e83397d6e8e355
sbkr74 <sbkr74@gmail.com>
2024-07-08 20:55:22 +0530

    exercise3.py file updated1.1(sample_list())

--- 6.Modules/Exercise/exercise3.py
+++ 6.Modules/Exercise/exercise3.py
@@ -1,12 +1,19 @@
 from random import shuffle as sh
 
-
+# modifying the original list
 def shuffle_list(inList:list):
     sh(inList)
     return inList
+# without modifying original list
+def sample_list(userList:list):
+    return(userList)
 
 if __name__ == '__main__':
     myList = [1,2,3,4,5]
-    newList = shuffle_list(myList)
-    print(newList)
+    shuffleList = shuffle_list(myList)
+    print(shuffleList)
+
+    print(sample_list(myList))
+
+
 

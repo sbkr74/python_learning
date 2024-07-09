ca0c64b7c4f3839e015bc8452cf5236755c81b4f
sbkr74 <sbkr74@gmail.com>
2024-07-09 18:19:55 +0530

    even-odd generated using list comp

--- 1.Basic/1.5.List/list_comp.py
+++ 1.Basic/1.5.List/list_comp.py
@@ -17,3 +17,9 @@ string = 'Python'
 strList = [i for i in string]
 print(strList)
 
+# for generating even number and odd number
+even = [num for num in range(1,21) if num%2==0]
+print(even)
+
+odd = [num for num in range(1,20) if num%2!=0]
+print(odd)

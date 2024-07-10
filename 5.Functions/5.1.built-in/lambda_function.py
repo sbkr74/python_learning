a8d25943c5e85c646b72559460173a31f658b891
sbkr74 <sbkr74@gmail.com>
2024-07-10 12:32:06 +0530

    lamda_function.py file updated1.0(lambda fun in another fun)

--- 5.Functions/5.1.built-in/lambda_function.py
+++ 5.Functions/5.1.built-in/lambda_function.py
@@ -12,3 +12,14 @@ print((lambda a,b:a+b)(2,3))    # output: 5
 mult_var = lambda a,b,c:a**2-3*b+4*c
 print(mult_var(5,5,3))          # output: 22
 
+#########################################################
+# Lambda function inside Another function
+
+def power(x):
+    return lambda n:x**n
+
+cube = power(4)(3)
+print(cube)                     # output: 64
+
+two_pow_five = power(2)(5)
+print(two_pow_five)             # output: 32

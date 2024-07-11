a0077625c7ab350136f72f092baeecde006c9e27
sbkr74 <sbkr74@gmail.com>
2024-07-11 18:39:38 +0530

    updated1.0 with function as return value

--- Advance/higher_order_function.py
+++ Advance/higher_order_function.py
@@ -9,3 +9,34 @@ def higher_order_function(f,lst):
 result = higher_order_function(sum_of_num,[1,2,3,4,5])
 print(result)
 
+# function as return value
+def square(x):
+    return x**2
+
+def cube(x):
+    return x**3
+
+def abs(x):
+    if x>0:
+        return x
+    else:
+        return -(x)
+
+def higher_order_function(type):
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'abs':
+        return abs
+    else:
+        return 'function not defined'
+
+result = higher_order_function('square')
+print(result(3))
+result = higher_order_function('cube')
+print(result(3))
+result = higher_order_function('abs')
+print(result(-3))
+result = higher_order_function('xyz')
+print(result(3))

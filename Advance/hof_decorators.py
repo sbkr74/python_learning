8c16790371888ceaee6da93530765c4f9490c250
sbkr74 <sbkr74@gmail.com>
2024-07-12 19:58:02 +0530

    updated1.0 (multi-decorators)

--- Advance/hof_decorators.py
+++ Advance/hof_decorators.py
@@ -22,3 +22,27 @@ def greet():
     return "Welcome to Learning Python"
 
 print(greet())
+
+###############################################################
+# Applying multiple decorator in single function
+
+def case_mod(f):
+    def top():
+        func = f()
+        mod = func.upper()
+        return mod
+    return top
+
+def splitting(f):
+    def gap():
+        func = f()
+        gapped = func.split()
+        return gapped
+    return gap
+
+@splitting
+@case_mod
+def compliment():
+    return 'It was good interaction.'
+
+print(compliment())

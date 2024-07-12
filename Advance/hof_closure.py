ee502519f5618db6a29b4bbcbb6610c140c57f8a
sbkr74 <sbkr74@gmail.com>
2024-07-12 19:41:31 +0530

    updated1.0 (adder function added)

--- Advance/hof_closure.py
+++ Advance/hof_closure.py
@@ -1,4 +1,5 @@
 # Closure: Python allows a nested function to access the outer scope of the enclosing function.
+# power function
 def power():
     x = int(input("Enter power of num: "))
     def sq(num):
@@ -6,5 +7,13 @@ def power():
     return sq
 
 closure_result = power()
-print(closure_result(2))
+print(f"power:",closure_result(2))
 
+# adder function
+def adder(x):
+    def adding(y):
+        return x+y
+    return adding
+
+addition = adder(5)
+print("adder:",addition(7))

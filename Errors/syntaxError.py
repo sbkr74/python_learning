7a73853db920a631249df03f2d5eaeb87a008302
sbkr74 <sbkr74@gmail.com>
2024-08-27 22:49:25 +0530

    Errors/syntaxError.py file added

---
+++ Errors/syntaxError.py
@@ -0,0 +1,10 @@
+'''
+>>> print 'hello world'
+  File "<stdin>", line 1
+    print 'hello world'
+                      ^
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print('hello world')?
+>>>
+'''
+
+print 'hello world'

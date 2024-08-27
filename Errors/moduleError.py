ea9dfe60f9c37b5230029828aac5ffa9cb0421a6
sbkr74 <sbkr74@gmail.com>
2024-08-27 22:53:03 +0530

    Errors/moduleError.py file added

---
+++ Errors/moduleError.py
@@ -0,0 +1,15 @@
+'''
+>>> import maths
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ModuleNotFoundError: No module named 'maths'
+>>>
+'''
+# 
+import maths
+
+'''
+In the example above, 
+I added an extra s to math deliberately and ModuleNotFoundError was raised. 
+Lets fix it by removing the extra s from math.
+'''

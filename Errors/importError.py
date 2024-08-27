70d82586fe3c5bbadfe435ddeda37df85bdd5472
sbkr74 <sbkr74@gmail.com>
2024-08-27 23:00:10 +0530

    Errors/importError.py file added

---
+++ Errors/importError.py
@@ -0,0 +1,9 @@
+'''
+>>> from math import power
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ImportError: cannot import name 'power' from 'math'
+>>>
+'''
+
+from math import power

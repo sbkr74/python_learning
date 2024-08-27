743b3e0f6a3899150eaa93a9b93a7936abc7b90f
sbkr74 <sbkr74@gmail.com>
2024-08-27 22:56:21 +0530

    Errors/attributeError.py file added

---
+++ Errors/attributeError.py
@@ -0,0 +1,11 @@
+'''
+>>> import math
+>>> math.PI
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: module 'math' has no attribute 'PI'
+>>>
+'''
+
+import math
+math.PI

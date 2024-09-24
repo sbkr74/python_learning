b8f7967559de2bc158524aafe74d155175d7e1c9
sbkr74 <sbkr74@gmail.com>
2024-09-24 20:07:50 +0530

    1.key_press.py file added

---
+++ Advance/keyboard_mod/1.key_press.py
@@ -0,0 +1,11 @@
+import keyboard
+
+# Simulate pressing and releasing a key
+keyboard.press('p')
+keyboard.press('y')
+keyboard.press('t')
+keyboard.press('h')
+keyboard.press('o')
+keyboard.press('n')
+
+keyboard.release('n')

81c6edb5b6cb4aea691b5df2d6f6e63e1b9d76ca
sbkr74 <sbkr74@gmail.com>
2025-01-13 23:17:31 +0530

    update2.0[date,time]

--- 6.Modules/built-in-mod/date_time/date_time.py
+++ 6.Modules/built-in-mod/date_time/date_time.py
@@ -38,3 +38,36 @@ print("time one:", time_one)
 time_two = now.strftime("%d/%m/%Y, %H:%M:%S")
 # dd/mm/YY H:M:S format
 print("time two:", time_two)
+
+# String to Time Using strptime
+from datetime import datetime
+date_string = "5 December, 2019"
+print("date_string =", date_string)
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+print("date_object =", date_object)
+
+# Using date
+from datetime import date
+d = date(2020, 1, 1)
+print(d)
+print('Current date:', d.today())    # 2019-12-05
+# date object of today's date
+today = date.today()
+print("Current year:", today.year)   # 2019
+print("Current month:", today.month) # 12
+print("Current day:", today.day)     # 5
+
+# Using time
+from datetime import time
+# time(hour = 0, minute = 0, second = 0)
+a = time()
+print("a =", a)
+# time(hour, minute and second)
+b = time(10, 30, 50)
+print("b =", b)
+# time(hour, minute and second)
+c = time(hour=10, minute=30, second=50)
+print("c =", c)
+# time(hour, minute, second, microsecond)
+d = time(10, 30, 50, 200555)
+print("d =", d)

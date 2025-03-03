aeb1e0b5d398db0394b7133293ffde93406c93ed
sbkr74 <sbkr74@gmail.com>
2025-03-03 21:10:25 +0530

    update1.0[handling using known execptions]

--- 7.Exception_Handling/7.1.try_except.py
+++ 7.Exception_Handling/7.1.try_except.py
@@ -18,3 +18,28 @@ except:
 In above both cases, the exception block will run and we do not know exactly the problem. 
 To analyze the problem, we can use the different error types with except.
 '''
+# enhancement in basic try-except [example 1]
+try:
+    print(10 + 'x')
+except Exception as e:
+    print(f"operation gone wrong. due to {e}.")
+
+# handling error if you already have idea what could go wrong.
+try:
+    print(10 + 'x')
+except TypeError:
+    print("Type Error occured.")
+
+# some cases for example 2
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you were born:')
+    age = 2019 - year_born
+    print(f'You are {name} and your age is {age}.')
+except TypeError:
+    print('Type error occured')
+except ValueError:
+    print('Value error occured')
+except ZeroDivisionError:
+    print('zero division error occured')
+

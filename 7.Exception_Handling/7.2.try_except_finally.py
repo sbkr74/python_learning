5c7688293756339d00ab863b3e73343fdc5d64ce
sbkr74 <sbkr74@gmail.com>
2025-03-28 19:55:50 +0530

    update1.0 [using else]

--- 7.Exception_Handling/7.2.try_except_finally.py
+++ 7.Exception_Handling/7.2.try_except_finally.py
@@ -8,3 +8,16 @@ except Exception as e:
     print("Error:",e)
 finally:
     print("I will always run.")
+
+
+try:
+    name = input("Enter Your Name: ")
+    year_of_grad = input("Enter year you wore graduated: ")
+    exp = date.today().year - int(year_of_grad)
+    print(f"Hello {name}, you have graduated {exp} years ago.")
+except Exception as e:
+    print("Error:",e)
+else:
+    print("Getting used to work with try block")
+finally:
+    print("I will always run.")

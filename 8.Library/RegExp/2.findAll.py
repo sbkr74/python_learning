824fef4b0db1d3655b304c6a3abb2edfe809ce4a
sbkr74 <sbkr74@gmail.com>
2025-07-13 11:12:48 +0530

    update1.0 (minor issues)

--- 8.Library/RegExp/2.findAll.py
+++ 8.Library/RegExp/2.findAll.py
@@ -4,3 +4,16 @@ import re
 words = "fear, gear, tear, tire"
 all_matches = re.findall(".ear",words)
 print(all_matches)
+
+# pattern #1
+sentence = "My phone number is 1234560987"
+numbers = re.findall(r"\d",sentence)
+print(numbers)
+
+'''Extracting phone numbers'''
+num = 1
+# numbers = int(numbers)
+for i in numbers:
+    i = int(i)
+    i=i*10 + (numbers[i+1])
+    print(i)

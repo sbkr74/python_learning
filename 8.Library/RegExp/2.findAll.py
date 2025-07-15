3d2ee56478ac299bddfc59b17b25c0e618a99150
sbkr74 <sbkr74@gmail.com>
2025-07-15 10:16:03 +0530

    update3.0 [new patterns]

--- 8.Library/RegExp/2.findAll.py
+++ 8.Library/RegExp/2.findAll.py
@@ -5,7 +5,13 @@ words = "fear, gear, tear, tire"
 all_matches = re.findall(".ear",words)
 print(all_matches)
 
-# pattern #1
+# pattern #1 [Literal Match]
+text = "apple banana apple grape"
+matches = re.findall(r"apple", text)
+print(matches)  # ['apple', 'apple']
+
+# pattern #2 [Matching Digit]
+# Example #1
 sentence = "My phone number is 1234560987"
 numbers = re.findall(r"\d",sentence)
 
@@ -15,3 +21,13 @@ contact=0
 for i in numbers:
     contact=contact*10+i
 print("Contact Number: ",contact)
+
+'''Normally extracting numbers'''
+num = re.findall(r"\d+",sentence)
+print(num)
+
+# Example #2
+text = "Order numbers: 0567, 4122, 210"
+matches = re.findall(r"\d+", text)
+print(matches)  # ['0567', '4122', '210']
+

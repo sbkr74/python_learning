eb4170dc000558327b2d44d87c9643ca5da2fe57
sbkr74 <sbkr74@gmail.com>
2025-07-15 21:57:22 +0530

    update4.0 [unlocked pattern #7]

--- 8.Library/RegExp/2.findAll.py
+++ 8.Library/RegExp/2.findAll.py
@@ -31,3 +31,32 @@ text = "Order numbers: 0567, 4122, 210"
 matches = re.findall(r"\d+", text)
 print(matches)  # ['0567', '4122', '210']
 
+# Pattern #3 [words starting with Capital letter]
+text = "aAlice went to Wonderland with Bob and Charlie"
+pattern = r"[A-Z][a-z]+"
+pattern1 = r"\b[A-Z][a-z]+\b"       #setting boundaries (setting first and last character)
+print(re.findall(pattern, text))
+print(re.findall(pattern1, text))
+
+# Pattern #4 [Matching Words Starting ("ba")]
+text = "I have ball, bat, basketball, football and boost."
+pattern = r"\bba[a-z]+\b"
+pattern = r"\bba\w+\b"
+print(re.findall(pattern,text))
+
+# Pattern #5 [Matching Words Ending ("ing")]
+text = "I am running, jumping and swimming today."
+pattern = r"\b\w+ing\b"
+print(re.findall(pattern, text))
+
+# Pattern #6 [Matching Dates (dd-mm-yyyy)]
+text = "Today is 15-07-2025. Another date is 01-01-2026."
+pattern = r"\b\d{2}-\d{2}-\d{4}\b"
+print(re.findall(pattern, text))
+
+# Pattern #7 [Extacting Email Address]
+text = "Contact us at support@example.com or sales@myshop.org or nothing1@gov.org or aV_1g@works.co"
+pattern = r"[a-z]+@[a-z]+.[a-z]+"
+mod_pattern = r"[a-z0-9A-Z_.+-]+@[a-z0-9A-Z_.+-]+\.[a-z0-9A-Z_.+-]+"
+print(re.findall(pattern, text))
+print(re.findall(mod_pattern, text))
